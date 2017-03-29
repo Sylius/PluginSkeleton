6fc3562c8a58c0095a5cd492c9684ac7882ebbdf
Łukasz <lchrusciel@gmail.com>
2017-03-29 09:33:29 +0200

    [Maintenance] Rename to SyliusPlugin

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acme\ExampleBundle\DependencyInjection;
+namespace Acme\ExamplePlugin\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
