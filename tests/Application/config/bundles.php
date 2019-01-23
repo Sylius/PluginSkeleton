6d3d5939401dcd9c3940d8cc6e13311428b9fe61
Mateusz Zalewski <zaleslaw@gmail.com>
2019-01-23 10:11:44 +0100

    Use SymfonyExtension ^2.0

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -56,4 +56,5 @@ return [
     Acme\SyliusExamplePlugin\AcmeSyliusExamplePlugin::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true, 'test_cached' => true],
 ];
