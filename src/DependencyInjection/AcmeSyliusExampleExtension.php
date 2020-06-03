86d60b589d5cfed34e6c961ac645900d170a9549
Kamil Kokot <kamil@kokot.me>
2020-06-03 14:13:03 +0200

    Introduce Psalm and fix reported errors

--- src/DependencyInjection/AcmeSyliusExampleExtension.php
+++ src/DependencyInjection/AcmeSyliusExampleExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Acme\SyliusExamplePlugin\DependencyInjection;
 
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -11,14 +12,16 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class AcmeSyliusExampleExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $config, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $loader->load('services.xml');
+    }
+
+    public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
+    {
+        return new Configuration();
     }
 }
