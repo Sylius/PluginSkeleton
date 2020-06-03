7a8a96743b33956336babd12344fb98dbdcd28d2
IDCT Bartosz Pachołek <bartosz@idct.pl>
2020-06-03 20:49:40 +0200

    Keep existing `node_modules` if link or dir, fallback to Junctions on Windows

--- bin/create_node_symlink.php
+++ bin/create_node_symlink.php
@@ -1,19 +1,45 @@
 <?php
 
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    echo '> This system is running Windows, we shall try to create a link and if it fails then a junction:' . PHP_EOL;
-    exec('mklink /D node_modules tests\\Application\\node_modules 2> NUL', $output, $returnCode);
-    if ($returnCode !== 0) {
-        echo '> Creation of Link failed; you can try with Admin privileges; trying to create a Junction:' . PHP_EOL;
-        exec('mklink /J node_modules tests\\Application\\node_modules 2> NUL', $output, $returnCode);
-    }
+const NODE_MODULES_FOLDER_NAME = 'node_modules';
+const PATH_TO_NODE_MODULES = 'tests' . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR . 'node_modules';
 
-    if ($returnCode === 0) {
-        echo "> Successfully created a Link/Junction." . PHP_EOL;
+/* cannot use `file_exists` or `stat` as gives false on symlinks if target path does not exist yet */
+if (@lstat(NODE_MODULES_FOLDER_NAME))
+{
+    if (is_link(NODE_MODULES_FOLDER_NAME) || is_dir(NODE_MODULES_FOLDER_NAME)) {
+        echo '> `' . NODE_MODULES_FOLDER_NAME . '` already exists as a link or folder, keeping existing as may be intentional.' . PHP_EOL;
+        exit(0);
     } else {
-        echo "> Failed with return code: " . $returnCode;
+        echo '> Invalid symlink `' . NODE_MODULES_FOLDER_NAME . '` detected, recreating...' . PHP_EOL;
+        if (!@unlink(NODE_MODULES_FOLDER_NAME)) {
+            echo '> Could not delete file `' . NODE_MODULES_FOLDER_NAME . '`.' . PHP_EOL;
+            exit(1);
+        }
     }
-} else {
-    echo '> This is a server not running Windows, we shall try to use a Symlink' . PHP_EOL;
-    passthru('ln -s tests/Application/node_modules node_modules');
 }
+
+/* try to create the symlink using PHP internals... */
+$success = @symlink(PATH_TO_NODE_MODULES, NODE_MODULES_FOLDER_NAME);
+
+/* if case it has failed, but OS is Windows... */
+if (!$success && strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    /* ...then try a different approach which does not require elevated permissions and folder to exist */
+    echo '> This system is running Windows, creation of links requires elevated privileges,' . PHP_EOL;
+    echo '> and target path to exist. Fallback to NTFS Junction:' . PHP_EOL;
+    exec(sprintf('mklink /J %s %s 2> NUL', NODE_MODULES_FOLDER_NAME, PATH_TO_NODE_MODULES), $output, $returnCode);
+    $success = $returnCode === 0;
+    if (!$success) {
+	echo '> Failed o create the required symlink' . PHP_EOL;
+        exit(2);
+    }
+}
+
+$path = @readlink(NODE_MODULES_FOLDER_NAME);
+/* check if link points to the intended directory */
+if ($path && realpath($path) === realpath(PATH_TO_NODE_MODULES)) {
+    echo '> Successfully created the symlink.' . PHP_EOL;
+    exit(0);
+}
+
+echo '> Failed to create the symlink to `' . NODE_MODULES_FOLDER_NAME . '`.' . PHP_EOL;
+exit(3);
