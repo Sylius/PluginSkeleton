b5b51a1b372a0004fda98c9fccf5e05ff06abaad
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2022-09-26 10:21:24 +0200

    Add Sylius 1.12 support

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -57,4 +57,6 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
 ];
