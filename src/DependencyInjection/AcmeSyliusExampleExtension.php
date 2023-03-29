f7d57bb3281b04d79b2e8279098be56b46202797
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2023-03-29 13:53:03 +0200

    Fix Doctrine Migrations

--- src/DependencyInjection/AcmeSyliusExampleExtension.php
+++ src/DependencyInjection/AcmeSyliusExampleExtension.php
@@ -4,27 +4,44 @@ declare(strict_types=1);
 
 namespace Acme\SyliusExamplePlugin\DependencyInjection;
 
-use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
+use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class AcmeSyliusExampleExtension extends Extension
+final class AcmeSyliusExampleExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
-    /**
-     * @psalm-suppress UnusedVariable
-     */
+    use PrependDoctrineMigrationsTrait;
+
+    /** @psalm-suppress UnusedVariable */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
 
         $loader->load('services.xml');
     }
 
-    public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
+    public function prepend(ContainerBuilder $container): void
     {
-        return new Configuration();
+        $this->prependDoctrineMigrations($container);
+    }
+
+    protected function getMigrationsNamespace(): string
+    {
+        return 'DoctrineMigrations';
+    }
+
+    protected function getMigrationsDirectory(): string
+    {
+        return '@AcmeSyliusExamplePlugin/migrations';
+    }
+
+    protected function getNamespacesOfMigrationsExecutedBefore(): array
+    {
+        return [
+            'Sylius\Bundle\CoreBundle\Migrations',
+        ];
     }
 }
