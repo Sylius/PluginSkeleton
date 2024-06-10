3eecd5b5283ae3f626a471780d77742b92b541b2
camilleislasse <camille.islasse@acseo-conseil.fr>
2024-06-10 18:42:38 +0200

    Delete useless things

--- ecs.php
+++ ecs.php
@@ -8,7 +8,6 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([
         __DIR__ . '/src',
-        __DIR__ . '/tests/Behat',
         __DIR__ . '/ecs.php',
     ]);
 
