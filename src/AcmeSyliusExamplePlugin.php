a34414ad0f743cbf78f4772f74ced49220cc52ae
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2025-06-27 11:50:12 +0200

    [Maintenance] cr fixes

--- src/AcmeSyliusExamplePlugin.php
+++ src/AcmeSyliusExamplePlugin.php
@@ -5,17 +5,11 @@ declare(strict_types=1);
 namespace Acme\SyliusExamplePlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class AcmeSyliusExamplePlugin extends Bundle
 {
     use SyliusPluginTrait;
-
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-    }
 
     public function getPath(): string
     {
