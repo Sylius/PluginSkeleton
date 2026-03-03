ac90dc76c2e374255d625a53654e946ac271ec2d
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2026-03-03 15:25:57 +0100

    Fix CI

--- config/routes/shop.php
+++ config/routes/shop.php
@@ -8,14 +8,14 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 return static function (RoutingConfigurator $routes): void {
     $routes->add('acme_sylius_example_static_welcome', '/static-welcome/{name}')
         ->defaults([
-            '_controller' => GreetingController::staticallyGreetAction(),
+            '_controller' => [GreetingController::class, 'staticallyGreetAction'],
             'name' => null,
         ])
     ;
 
     $routes->add('acme_sylius_example_dynamic_welcome', '/dynamic-welcome/{name}')
         ->defaults([
-            '_controller' => GreetingController::dynamicallyGreetAction(),
+            '_controller' => [GreetingController::class, 'dynamicallyGreetAction'],
             'name' => null,
         ])
     ;
