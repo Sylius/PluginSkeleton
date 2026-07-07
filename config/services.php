2e7ecae4163f98b176a5dd6eecb75371cb281544
Simon Krull <s.krull@bitexpert.de>
2026-07-07 10:19:34 +0200

    [Maintenance] [XML2PHP] convert service config from .xml to .php

---
+++ config/services.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function (ContainerConfigurator $container) {
+    $services = $container->services();
+    $container->import('services/**');
+
+
+    $services->set(\Acme\SyliusExamplePlugin\Controller\GreetingController::class)
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+};
